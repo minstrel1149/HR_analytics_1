14a390e1e1e6ae86f70b4072b2ba3eea153f5c7c
Taehwa Kim <kth1149@gmail.com>
2023-07-23 17:21:08 +0900

    [Update] EDA Processing

--- data/utils.py
+++ data/utils.py
@@ -5,10 +5,10 @@ def feature_classification(df, classify='character'):
     if classify == 'character':
         char_dict = {'ID':['EmployeeNumber'],
                      'Compensation':['DailyRate', 'HourlyRate', 'MonthlyRate', 'MonthlyIncome', 'PercentSalaryHike', 'StockOptionLevel'],
-                     'Biodata':['Age', 'Over18', 'Gender', 'MaritalStatus', 'Education', 'EducationField', 
+                     'Biodata':['Age', 'Gender', 'MaritalStatus', 'Education', 'EducationField', 
                                 'DistanceFromHome', 'NumCompaniesWorked', 'TotalWorkingYears'],
                      'Work':['Department', 'BusinessTravel', 'JobLevel', 'JobRole', 'JobInvolvement', 'OverTime', 
-                             'WorkLifeBalance', 'StandardHours', 'TrainingTimesLastYear', 'YearsAtCompany', 
+                             'WorkLifeBalance', 'TrainingTimesLastYear', 'YearsAtCompany', 
                              'YearsInCurrentRole', 'YearsSinceLastPromotion', 'YearsWithCurrManager'],
                      'result':['EnvironmentSatisfaction', 'JobSatisfaction', 'PerformanceRating', 'RelationshipSatisfaction', 'Attrition']}
         return char_dict
